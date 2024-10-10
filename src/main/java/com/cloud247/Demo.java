13c095be4d91897168f7c336407e4516e299d992
Iftequar Shaikh <80765083+Iftequar7@users.noreply.github.com>
2024-10-10 17:51:25 +0530

    Update Demo.java

--- src/main/java/com/cloud247/Demo.java
+++ src/main/java/com/cloud247/Demo.java
@@ -58,7 +58,7 @@ public class Demo extends HttpServlet {
         	Class.forName("com.mysql.jdbc.Driver");
 
             // Establish connection
-            connection = DriverManager.getConnection("jdbc:mysql://172.17.0.2:3306/cv01", "root", "Admin@123");
+            connection = DriverManager.getConnection("jdbc:mysql://database-1.cvom04u6egkb.us-east-1.rds.amazonaws.com:3306/cv01", "admin", "Master!234");
 
             // Create SQL insert statement
             String sql = "INSERT INTO bill_manage (bill_number, units, amount, generated_at) VALUES (?, ?, ?, ?)";
